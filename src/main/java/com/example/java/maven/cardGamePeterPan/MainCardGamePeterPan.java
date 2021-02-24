f02ad59e4df3dadf6799b5380c038ef0f6ac407e
matsarat <mat.saratowicz@gmail.com>
2021-02-24 12:03:43 +0100

    STEP 0 - app creates players, deals cards, display hands

---
+++ src/main/java/com/example/java/maven/cardGamePeterPan/MainCardGamePeterPan.java
@@ -0,0 +1,30 @@
+package com.example.java.maven.cardGamePeterPan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainCardGamePeterPan {
+
+    public static void main(String[] args) {
+        int numberOfPlayers = 2;
+        MessagePrinter messagePrinter = new MessagePrinter();
+        UserInputProvider userInputProvider = new UserInputProvider(messagePrinter);
+
+        List<Player> players = new ArrayList<>();
+        Deck deck = new Deck();
+
+        for (int i = 1; i <= numberOfPlayers; i++) {
+            messagePrinter.printMessage("Player " + i);
+            players.add(new Player(userInputProvider.getPlayersName()));
+        }
+
+        Game game = new Game(userInputProvider,
+                messagePrinter,
+                players,
+                deck);
+
+
+        game.dealCards();
+        game.printPlayers();
+    }
+}
