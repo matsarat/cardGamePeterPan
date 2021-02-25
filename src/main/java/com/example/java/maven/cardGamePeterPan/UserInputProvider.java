a6b980bbe9fbf6f990bdefb94b16f2f45d34ab25
matsarat <mat.saratowicz@gmail.com>
2021-02-25 19:49:28 +0100

    Add validation for integer input

--- src/main/java/com/example/java/maven/cardGamePeterPan/UserInputProvider.java
+++ src/main/java/com/example/java/maven/cardGamePeterPan/UserInputProvider.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserInputProvider {
     private final MessagePrinter messagePrinter;
     private final static String ASK_FOR_NAME = "What is your name? ";
-    private final static String ASK_FOR_CARD = "Which card would you like to draw?";
+    private final static String INPUT_MUST_BE_AN_INTEGER = "Your input must be an integer! Try again!";
 
     public UserInputProvider(MessagePrinter messagePrinter) {
         this.messagePrinter = messagePrinter;
@@ -18,8 +18,13 @@ public class UserInputProvider {
     }
 
     public int getNumberOfChosenCard() {
-        messagePrinter.printMessage(ASK_FOR_CARD);
         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        try {
+            int numberOfChosenCard = Integer.parseInt(scanner.nextLine());
+            return numberOfChosenCard;
+        }catch(NumberFormatException e) {
+            messagePrinter.printError(INPUT_MUST_BE_AN_INTEGER);
+            return getNumberOfChosenCard();
+        }
     }
 }
