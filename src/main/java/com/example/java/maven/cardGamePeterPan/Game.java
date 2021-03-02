1224ec305e8f53ebce332d223f5232d0008a028f
matsarat <mat.saratowicz@gmail.com>
2021-03-02 12:43:06 +0100

    Apply getting hand size without using getter

--- src/main/java/com/example/java/maven/cardGamePeterPan/Game.java
+++ src/main/java/com/example/java/maven/cardGamePeterPan/Game.java
@@ -1,11 +1,12 @@
 package com.example.java.maven.cardGamePeterPan;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Game {
     private final UserInputProvider userInputProvider;
     private final MessagePrinter messagePrinter;
-    private final ArrayList<Player> playerList;
+    private final List<Player> playerList;
     private final Deck deck;
 
     private final static String ASK_FOR_CARDS_TO_DISCARD= """
@@ -21,7 +22,7 @@ public class Game {
 
     public Game(UserInputProvider userInputProvider,
                 MessagePrinter messagePrinter,
-                ArrayList<Player> playerList,
+                List<Player> playerList,
                 Deck deck) {
         this.userInputProvider = userInputProvider;
         this.messagePrinter = messagePrinter;
@@ -67,39 +68,61 @@ public class Game {
 
     public int getValidCardIndex(Player player) {
         int userInput = userInputProvider.getNumberOfChosenCard();
-        if (userInput > 0 && userInput <= player.getHand().size()) {
+        if (userInput > 0 && userInput <= player.getHandSize()) {
             return (userInput - 1);
         }
         else {
-            messagePrinter.printError(String.format("Your input must be an integer between 1 and %s", player.getHand().size()));
+            messagePrinter.printError(String.format("Your input must be an integer between 1 and %s", player.getHandSize()));
             return getValidCardIndex(player);
         }
     }
 
     public void getCardFromPreviousPlayerHand(Player player) {
         Player previousPlayer = getPreviousPlayer(player);
-        messagePrinter.printMessage(String.format("%s has %s cards. Which one would you like to draw?", previousPlayer.getName(), previousPlayer.getHand().size()));
+        messagePrinter.printMessage(String.format("%s has %s cards. Which one would you like to draw?", previousPlayer.getName(), previousPlayer.getHandSize()));
         player.takeCardFromAnotherPlayersHand(previousPlayer, getValidCardIndex(previousPlayer));
     }
 
 
     public Player getPreviousPlayer(Player player) {
-        int indexOfCurrentPlayer = playerList.indexOf(player);
+        List<Player> activePlayers = getActivePlayers();
+        int indexOfCurrentPlayer = activePlayers.indexOf(player);
         if ((indexOfCurrentPlayer - 1) < 0) {
-            return playerList.get(playerList.size() - 1);
+            return activePlayers.get(activePlayers.size() - 1);
         }
         else {
-            return playerList.get(indexOfCurrentPlayer-1);
+            return activePlayers.get(indexOfCurrentPlayer-1);
         }
     }
 
     public int countActivePlayers() {
         int activePlayerCounter = 0;
         for (Player player : playerList) {
-            if (player.isPlaying()) {
+            if (player.getHandSize() > 0) {
                 activePlayerCounter += 1;
             }
         }
         return activePlayerCounter;
+    }
+
+    public List<Player> getActivePlayers(){
+        return playerList.stream()
+                .filter(player -> player.getHandSize() > 0)
+                .collect(Collectors.toList());
+    }
+
+    public void play() {
+        while (countActivePlayers() > 1) {
+            for (Player player : getActivePlayers()) {
+                player.checkIfActive();
+                if (player.isPlaying()) {
+                    messagePrinter.printMessage(String.format("Your turn, %s", player.getName()));
+                    getCardFromPreviousPlayerHand(player);
+                    if (player.isDiscardingCardsPossible()) {
+                        choseCardsToDiscard(player);
+                    }
+                }
+            }
+        }
     }
 }
