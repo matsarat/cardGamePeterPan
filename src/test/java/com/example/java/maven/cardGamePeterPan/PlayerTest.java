2ddcd3ddb7cbd56dcdc9702e0594694bf7e8e4bf
matsarat <mat.saratowicz@gmail.com>
2021-03-04 16:17:02 +0100

    Add tests for areCardsMatching method and merge other two tests

--- src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
+++ src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
@@ -36,6 +36,8 @@ public class PlayerTest {
         Player anotherPlayer = new Player("Another");
         Card card = Mockito.mock(Card.class);
         anotherPlayer.addCardToHand(card);
+        int cardIndexOutOfBounds = 1;
+
 
 //        when
 
@@ -45,51 +47,67 @@ public class PlayerTest {
 
         assertThat(player.getHandSize()).isEqualTo(1);
         assertThat(anotherPlayer.getHandSize()).isEqualTo(0);
-
-    }
-
-    @Test
-    void shouldThrowExceptionIfChosenToTakeCardIndexIsOutOfBounds() {
-
-//        given
-        Player anotherPlayer = new Player("Another");
-        Card card = Mockito.mock(Card.class);
-        anotherPlayer.addCardToHand(card);
-        int cardIndexOutOfBounds = 1;
-
-//        then
         assertThrows(IndexOutOfBoundsException.class, () -> player.takeCardFromAnotherPlayersHand(anotherPlayer, cardIndexOutOfBounds));
+
+
     }
 
     @Test
-    void shouldReturnTrueIfPlayerHasTwoCardsMatchingBySuit() {
+    void shouldReturnTrueIfDiscardingBySuitIsPossible() {
 
 //        given
         Card card1 = new Card(Card.Suit.CLUB, Card.Rank.ACE);
         Card card2 = new Card(Card.Suit.SPADE, Card.Rank.JACK);
         Card card3 = new Card(Card.Suit.CLUB, Card.Rank.TEN);
 
+//        when
         player.addCardToHand(card1);
         player.addCardToHand(card2);
         player.addCardToHand(card3);
 
 //        then
         assertThat(player.isDiscardingCardsPossible()).isTrue();
+        assertThat(player.getHandSize()).isEqualTo(3);
+
     }
 
     @Test
-    void shouldReturnTrueIfPlayerHasTwoCardsMatchingByRank() {
+    void shouldReturnTrueIfDiscardingByRankIsPossible() {
 
 //        given
         Card card1 = new Card(Card.Suit.CLUB, Card.Rank.ACE);
         Card card2 = new Card(Card.Suit.SPADE, Card.Rank.JACK);
         Card card3 = new Card(Card.Suit.HEART, Card.Rank.ACE);
 
+//        when
         player.addCardToHand(card1);
         player.addCardToHand(card2);
         player.addCardToHand(card3);
 
 //        then
         assertThat(player.isDiscardingCardsPossible()).isTrue();
+        assertThat(player.getHandSize()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldReturnTrueIfCardsAreMatching() {
+
+//        given
+        Card card1 = new Card(Card.Suit.CLUB, Card.Rank.ACE);
+        Card card2 = new Card(Card.Suit.CLUB, Card.Rank.JACK);
+        Card card3 = new Card(Card.Suit.HEART, Card.Rank.ACE);
+
+
+//        when
+        player.addCardToHand(card1);
+        player.addCardToHand(card2);
+        player.addCardToHand(card3);
+
+//      then
+        assertThat(player.areCardsMatching(0, 1)).isTrue();
+        assertThat(player.areCardsMatching(0, 2)).isTrue();
+        assertThat(player.areCardsMatching(1, 2)).isFalse();
+        assertThrows(IndexOutOfBoundsException.class, () -> player.areCardsMatching(0, 4));
+        assertThrows(IllegalArgumentException.class, () -> player.areCardsMatching(0, 0));
     }
 }
