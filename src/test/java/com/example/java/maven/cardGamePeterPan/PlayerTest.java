2b46a6c507741538a1b1cbc373737a7656faf0dd
matsarat <mat.saratowicz@gmail.com>
2021-03-04 15:51:44 +0100

    Add tests for checking if discarding cards is possible

--- src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
+++ src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
@@ -58,6 +58,38 @@ public class PlayerTest {
         int cardIndexOutOfBounds = 1;
 
 //        then
-        assertThrows(IndexOutOfBoundsException.class, () -> player.takeCardFromAnotherPlayersHand(anotherPlayer, 1));
+        assertThrows(IndexOutOfBoundsException.class, () -> player.takeCardFromAnotherPlayersHand(anotherPlayer, cardIndexOutOfBounds));
+    }
+
+    @Test
+    void shouldReturnTrueIfPlayerHasTwoCardsMatchingBySuit() {
+
+//        given
+        Card card1 = new Card(Card.Suit.CLUB, Card.Rank.ACE);
+        Card card2 = new Card(Card.Suit.SPADE, Card.Rank.JACK);
+        Card card3 = new Card(Card.Suit.CLUB, Card.Rank.TEN);
+
+        player.addCardToHand(card1);
+        player.addCardToHand(card2);
+        player.addCardToHand(card3);
+
+//        then
+        assertThat(player.isDiscardingCardsPossible()).isTrue();
+    }
+
+    @Test
+    void shouldReturnTrueIfPlayerHasTwoCardsMatchingByRank() {
+
+//        given
+        Card card1 = new Card(Card.Suit.CLUB, Card.Rank.ACE);
+        Card card2 = new Card(Card.Suit.SPADE, Card.Rank.JACK);
+        Card card3 = new Card(Card.Suit.HEART, Card.Rank.ACE);
+
+        player.addCardToHand(card1);
+        player.addCardToHand(card2);
+        player.addCardToHand(card3);
+
+//        then
+        assertThat(player.isDiscardingCardsPossible()).isTrue();
     }
 }
