bb4be4b9390b5029c14837074e891653e7605840
matsarat <mat.saratowicz@gmail.com>
2021-03-04 15:43:54 +0100

    Add tests for taking card from another player's hand

--- src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
+++ src/test/java/com/example/java/maven/cardGamePeterPan/PlayerTest.java
@@ -3,7 +3,9 @@ package com.example.java.maven.cardGamePeterPan;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class PlayerTest {
@@ -25,5 +27,37 @@ public class PlayerTest {
 
 //        then
         assertThat(player.getHandSize()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldTakeCardFromAnotherPlayersHand() {
+
+//        given
+        Player anotherPlayer = new Player("Another");
+        Card card = Mockito.mock(Card.class);
+        anotherPlayer.addCardToHand(card);
+
+//        when
+
+        player.takeCardFromAnotherPlayersHand(anotherPlayer, 0);
+
+//        then
+
+        assertThat(player.getHandSize()).isEqualTo(1);
+        assertThat(anotherPlayer.getHandSize()).isEqualTo(0);
+
+    }
+
+    @Test
+    void shouldThrowExceptionIfChosenToTakeCardIndexIsOutOfBounds() {
+
+//        given
+        Player anotherPlayer = new Player("Another");
+        Card card = Mockito.mock(Card.class);
+        anotherPlayer.addCardToHand(card);
+        int cardIndexOutOfBounds = 1;
+
+//        then
+        assertThrows(IndexOutOfBoundsException.class, () -> player.takeCardFromAnotherPlayersHand(anotherPlayer, 1));
     }
 }
