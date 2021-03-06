fd65aecdf357d64166996cda8a3eab4b5ea86ae4
matsarat <mat.saratowicz@gmail.com>
2021-03-06 13:58:59 +0100

    Add test checking if deck contains Witch

--- src/test/java/com/example/java/maven/cardGamePeterPan/DeckTest.java
+++ src/test/java/com/example/java/maven/cardGamePeterPan/DeckTest.java
@@ -19,6 +19,7 @@ public class DeckTest {
 
 //        then
         assertThat(deck.getCards().size()).isEqualTo(25); // because we have 24 card in small deck (from 9 to Ace) + Witch
+        assertThat(deck.toString()).contains("WITCH");
     }
 
     @Test
