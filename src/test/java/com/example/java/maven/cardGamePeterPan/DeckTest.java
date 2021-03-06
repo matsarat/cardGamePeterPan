f02c87b842d73d3f7213c89543effe1e82235964
matsarat <mat.saratowicz@gmail.com>
2021-03-06 14:30:22 +0100

    Add test checking if cards are unique

--- src/test/java/com/example/java/maven/cardGamePeterPan/DeckTest.java
+++ src/test/java/com/example/java/maven/cardGamePeterPan/DeckTest.java
@@ -4,6 +4,9 @@ package com.example.java.maven.cardGamePeterPan;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeckTest {
@@ -20,6 +23,16 @@ public class DeckTest {
 //        then
         assertThat(deck.getCards().size()).isEqualTo(25); // because we have 24 card in small deck (from 9 to Ace) + Witch
         assertThat(deck.toString()).contains("WITCH");
+    }
+
+    @Test
+    void deckShouldContainUniqueElements() {
+
+//        given
+        Set<Card> cardSet = new HashSet<>(deck.getCards()); //because set can contain only unique elements
+
+//        then
+        assertThat(cardSet).size().isEqualTo(25);
     }
 
     @Test
